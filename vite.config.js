ada0d3726b5e4de01c3ef7b290c3eaf0462d3663
Jerry Bruwes <jbruwes@gmail.com>
2024-11-28 12:25:34 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ export default {
       formats: ["es"],
       name: "vue.worker.ts",
     },
+    resolve: {
+      alias: {
+        path: "path-browserify",
+      },
+    },
     rollupOptions: {
       output: {
         entryFileNames: "[name].js",
