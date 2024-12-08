5e3a31e0da322f63821c057ca199234e34563790
Jerry Bruwes <jbruwes@gmail.com>
2024-12-08 15:12:16 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,27 @@ import type { InputOption } from "rollup";
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-/** Path of library entry */
-const entry: InputOption = resolve(resolve("src"), "vue.worker.ts");
-/** Output bundle formats */
-const formats: LibraryFormats[] = ["es"];
-/**
- * The name of the package file output. The default file name is the name option
- * of the project package.json. It can also be defined as a function taking the
- * format as an argument.
- */
-const fileName: string = "[name]";
-/**
- * Build in library mode. The value should be the global name of the lib in UMD
- * mode. This will produce esm + cjs + umd bundle formats with default
- * configurations that are suitable for distributing libraries.
- */
-const lib: LibraryOptions = { entry, formats, fileName };
 /** Build specific options */
-const build: BuildEnvironmentOptions = { lib };
+const build = (() => {
+  const lib = (() => {
+    /** Path of library entry */
+    const entry = resolve(resolve("src"), "vue.worker.ts") as InputOption;
+    /** Output bundle formats */
+    const formats = ["es"] as LibraryFormats[];
+    /**
+     * The name of the package file output. The default file name is the name
+     * option of the project package.json. It can also be defined as a function
+     * taking the format as an argument.
+     */
+    const fileName = "[name]";
+    /**
+     * Build in library mode. The value should be the global name of the lib in
+     * UMD mode. This will produce esm + cjs + umd bundle formats with default
+     * configurations that are suitable for distributing libraries.
+     */
+    return { entry, formats, fileName } as LibraryOptions;
+  })();
+  return { lib } as BuildEnvironmentOptions;
+})();
+
 export default defineConfig({ build });
